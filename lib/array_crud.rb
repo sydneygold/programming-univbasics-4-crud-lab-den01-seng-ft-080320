d3e431acb2e5c00558057920359b52f5355ce0ab
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-16 20:12:10 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,49 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+["Joel", "Ellie", "Dina", "Jesse"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+[]
+array.push("arrays!")
+array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+[]
+array.unshift("wow")
+array
 end
 
 def remove_element_from_end_of_array(array)
-  
+[]
+array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+[]
+array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+[]
+array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+[]
+array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+[]
+array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+[]
+array[3] = "totally"
 end
